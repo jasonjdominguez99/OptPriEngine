bdb0c69faf784728a5ba8b1100b72e49e16f6b8a
Jason Dominguez <jasonjdominguez99@gmail.com>
2024-11-10 22:36:55 +0000

    refactor: reduce code repetition for calculating asset prices in `BinomialTreeModel`

--- src/BinomialTreeModel.cpp
+++ src/BinomialTreeModel.cpp
@@ -1,7 +1,6 @@
 #include "BinomialTreeModel.h"
 
 #include <cmath>
-#include <vector>
 
 BinomialTreeModel::BinomialTreeModel(const int numTimeSteps) :
     m_numTimeSteps(numTimeSteps > 0 ? numTimeSteps : 1) // silently enforce minimum number of time steps
@@ -19,13 +18,7 @@ double BinomialTreeModel::calculateCallPrice(const StrategyParameters strategyPa
 
     const size_t maxNumNodes = static_cast<size_t>(m_numTimeSteps + 1);
 
-    // Calculate possible asset prices at maturity
-    std::vector<double> assetPrices(maxNumNodes);
-    for (size_t i = 0; i < maxNumNodes; ++i)
-    {
-        // 0th element is the highest possible asset price
-        assetPrices[i] = S * std::pow(u, m_numTimeSteps - i) * std::pow(d, i);
-    }
+    std::vector<double> assetPrices = calculateAssetPrices(S, u, d, dt, maxNumNodes);
 
     // Initialize call prices at maturity
     std::vector<double> callPrices(maxNumNodes);
@@ -57,13 +50,7 @@ double BinomialTreeModel::calculatePutPrice(const StrategyParameters strategyPar
 
     const size_t maxNumNodes = static_cast<size_t>(m_numTimeSteps + 1);
 
-    // Calculate possible asset prices at maturity
-    std::vector<double> assetPrices(maxNumNodes);
-    for (size_t i = 0; i < maxNumNodes; ++i)
-    {
-        // 0th element is the highest possible asset price
-        assetPrices[i] = S * std::pow(u, m_numTimeSteps - i) * std::pow(d, i);
-    }
+    std::vector<double> assetPrices = calculateAssetPrices(S, u, d, dt, maxNumNodes);
 
     // Initialize put prices at maturity
     std::vector<double> putPrices(maxNumNodes);
@@ -82,4 +69,16 @@ double BinomialTreeModel::calculatePutPrice(const StrategyParameters strategyPar
     }
 
     return putPrices[0];
+}
+
+std::vector<double> BinomialTreeModel::calculateAssetPrices(const double S, const double u, const double d, const double dT, const size_t maxNumNodes) const
+{
+    std::vector<double> assetPrices(maxNumNodes);
+    for (size_t i = 0; i < maxNumNodes; ++i)
+    {
+        // 0th element is the highest possible asset price
+        assetPrices[i] = S * std::pow(u, m_numTimeSteps - i) * std::pow(d, i);
+    }
+
+    return assetPrices;
 }
--- src/BinomialTreeModel.h
+++ src/BinomialTreeModel.h
@@ -2,7 +2,7 @@
 
 #include "PricingStrategy.h"
 
-#include <cstddef>
+#include <vector>
 
 class BinomialTreeModel : public PricingStrategy
 {
@@ -12,6 +12,9 @@ public:
 
     double calculateCallPrice(const StrategyParameters strategyParams) override;
     double calculatePutPrice(const StrategyParameters strategyParams) override;
+
+private:
+    std::vector<double> calculateAssetPrices(const double S, const double u, const double d, const double dT, const size_t maxNumNodes) const;
 
 private:
     int m_numTimeSteps;
