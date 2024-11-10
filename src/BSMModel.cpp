59374b80408237f8864a58b3fb8f03df3ce1036e
Jason Dominguez <jasonjdominguez99@gmail.com>
2024-11-10 19:51:10 +0000

    refactor: use explicit tye instead of `auto` in `BSMModel`

--- src/BSMModel.cpp
+++ src/BSMModel.cpp
@@ -14,8 +14,8 @@ double BSMModel::calculateCallPrice(const StrategyParameters strategyParams)
 {
     auto [K, S, sigma, r, T] = strategyParams;
 
-    const auto d1 = (std::log(S / K) + (r + 0.5 * std::pow(sigma, 2)) * T) / (sigma * std::sqrt(T));
-    const auto d2 = d1 - sigma * sqrt(T);
+    const double d1 = (std::log(S / K) + (r + 0.5 * std::pow(sigma, 2)) * T) / (sigma * std::sqrt(T));
+    const double d2 = d1 - sigma * sqrt(T);
 
     return S * normalCDF(d1) - K * std::exp(-r * T) * normalCDF(d2);
 }
@@ -24,8 +24,8 @@ double BSMModel::calculatePutPrice(const StrategyParameters strategyParams)
 {
     auto [K, S, sigma, r, T] = strategyParams;
 
-    const auto d1 = (std::log(S / K) + (r + 0.5 * std::pow(sigma, 2)) * T) / (sigma * std::sqrt(T));
-    const auto d2 = d1 - sigma * sqrt(T);
+    const double d1 = (std::log(S / K) + (r + 0.5 * std::pow(sigma, 2)) * T) / (sigma * std::sqrt(T));
+    const double d2 = d1 - sigma * sqrt(T);
 
     return K * std::exp(-r * T) * normalCDF(-d2) - S * normalCDF(-d1);
 }
