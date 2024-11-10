591ef2940edbbd90d0833cc856ff6b8f0bbce89e
Jason Dominguez <jasonjdominguez99@gmail.com>
2024-11-10 19:27:27 +0000

    refactor: use explicit type names rather than `auto` for simple types

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,11 @@
 
 int main()
 {
-    const auto S = 100.0;
-    const auto K = 100.0;
-    const auto sigma = 0.2;
-    const auto r = 0.05;
-    const auto T = 1.0;
+    const double S = 100.0;
+    const double K = 100.0;
+    const double sigma = 0.2;
+    const double r = 0.05;
+    const double T = 1.0;
 
     const auto strategyParams = StrategyParameters { K, S, sigma, r, T };
 
@@ -25,15 +25,15 @@ int main()
 
     // Black-Scholes-Merton model
     std::cout << "\nBlack-Scholes-Merton model:" << std::endl;
-    const auto callPriceBSM = engine.calculateCallPrice(strategyParams);
+    const double callPriceBSM = engine.calculateCallPrice(strategyParams);
     std::cout << "Call price: " << callPriceBSM << std::endl;
 
     // Binomial tree model
     std::cout << "\nBinomial tree model:" << std::endl;
-    const auto numTimeSteps = 1000;
+    const size_t numTimeSteps = 1000;
     engine.setPricingStrategy(std::make_unique<BinomialTreeModel>(numTimeSteps));
     std::cout << "Number of time steps: " << numTimeSteps << std::endl;
-    const auto callPriceBinomial = engine.calculateCallPrice(strategyParams);
+    const double callPriceBinomial = engine.calculateCallPrice(strategyParams);
     std::cout << "Call price: " << callPriceBinomial << std::endl;
 
     return 0;
