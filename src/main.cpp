e67e2f67b96b53dcbd0b21f0903a8239cdc601f5
Jason Dominguez <jasonjdominguez99@gmail.com>
2024-11-10 22:20:44 +0000

    feat: change testing example

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,18 @@
 
 int main()
 {
-    const double S = 100.0;
     const double K = 100.0;
-    const double sigma = 0.2;
-    const double r = 0.05;
-    const double T = 1.0;
+    const double S = 100.0;
+    const double sigma = 0.33;
+    const double r = 0.03;
+    const double T = 1.0 / 12.0; // 1 month
 
     const auto strategyParams = StrategyParameters { K, S, sigma, r, T };
 
     // Display parameters
     std::cout << "Parameters:" << std::endl;
-    std::cout << "S: " << S << std::endl;
     std::cout << "K: " << K << std::endl;
+    std::cout << "S: " << S << std::endl;
     std::cout << "sigma: " << sigma << std::endl;
     std::cout << "r: " << r << std::endl;
     std::cout << "T: " << T << std::endl;
